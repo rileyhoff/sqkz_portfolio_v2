b39e4da4821feac24a5b72f81b2bce6d4655dceb
rileyhoff <rhoff@A-Higher-Power.localdomain>
2020-01-26 19:51:54 -0500

    redesign project with random picture intro

---
+++ script.js
@@ -0,0 +1,34 @@
+function displayItems() {
+    var url = document.URL;
+    var id = url.substring(url.lastIndexOf('#')+1);
+    document.getElementById(id).style.display = "block";
+    document.getElementById(id+"Nav").className = "active";
+}
+
+function mouseMove() {
+    z+=1;
+    //how fast to change items
+    if ((z%10)==1){
+      //when on first element set prev to last element
+      if(x==0){ 
+        prev = 34;
+      }else{
+      prev = x-1;
+      }
+      x+=1;
+      //number of items (including 0)
+      if(x>34){
+        x=0;
+      }
+      document.getElementById(x+3).className = "active";
+      document.getElementById(prev+3).className = "hidden";
+      console.log(x+3);
+    }
+    
+    document.getElementById("num").innerHTML = x;
+    document.getElementById("prev").innerHTML = prev+2;
+  }
+  //init
+  z=0;
+  x=0; //change back to 0 to use above function
+  prev = -1;
